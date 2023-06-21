f0631586f9d2642f9a6f0e14181f32895c5099f0
nagemd <137335693+nagemd@users.noreply.github.com>
2023-06-22 06:01:05 +1000

    Add files via upload

---
+++ src/settings.4b56d.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/00_home/home.fire",orientation:"",jsList:[],bundleVers:{internal:"1b082",main:"726f3"}};
